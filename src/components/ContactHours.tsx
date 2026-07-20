66fa59d6d7424e73988cb23a56b1e34c5d5a4b09
emeraldtorst <emeraldtorstein@gmail.com>
2026-07-20 15:01:46 +0200

    Set Contact section Lottie animation to LVmd5JB8nz.json

--- src/components/ContactHours.tsx
+++ src/components/ContactHours.tsx
@@ -32,7 +32,7 @@ export const ContactHours: React.FC = () => {
         <div className="section-title animate-slide-up" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <div style={{ marginBottom: '8px' }}>
             <LottieAnimation
-              url="https://lottie.host/8e6862f5-71f4-4ab1-b783-2ff842ea97d8/szV4j1T5vz.json"
+              url="https://lottie.host/02e5eb5c-5613-41a1-b0db-4dc732ae71e6/LVmd5JB8nz.json"
               width="85px"
               height="85px"
             />
